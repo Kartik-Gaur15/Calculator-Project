4ad44c814d010871a0cfd04e7ca4b4aed90cb061
Kartik-Gaur15 <74137221+Kartik-Gaur15@users.noreply.github.com>
2025-02-02 11:08:45 +0530

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+int main()
+{
+    int a=0,b=0,c=0,d=0;
+    std::cout<<"Welcoke to the simple calculator your request is our commnd";
+    std::cout<<"\n";
+    std::cout<<"Enter 2 numbers to start with the opertions";
+    std::cin >> a>>b;
+    std::cout<<"Press 1 for Addition (+)";
+    std::cout<<"Press 2 for Substraction (-)";
+    std::cout<<"Press 3 for Multiplication (*)";
+    std::cout<<"Press 4 for Division (/)";
+    std::cin >> c;
+    if(c==1)
+    {
+        d=a+b;
+        std::cout<<d;
+    }
+    if(c==2)
+    {
+        if(a>b)
+        {
+            d=a-b;
+            std::cout<<d;
+        }
+        else
+        {
+            d=b-a;
+            std::cout<<d;
+        }
+    }
+    if(c==3)
+    {
+        d=a*b;
+        std::cout<<d;
+    }
+    if(c==4)
+    {
+        d=a/b;
+        std::cout<<d;
+    }
+    
+
+    return 0;
+}
